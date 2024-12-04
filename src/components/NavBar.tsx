3e9a117ec701ad0a19fc596ecb42b47351c9900c
Arifin Cahyadi Santoso <arifincahyadisantoso@Arifins-MacBook-Pro-2.local>
2024-12-04 12:59:47 +0700

    try SPA

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,18 @@
+import { Link } from "react-router";
+
 export default function NavBar() {
   return (
     <>
       <nav>
         <ul className="flex gap-10 justify-center pt-5 pb-5 text-2xl font-thin bg-neutral-800 text-white">
           <li className=" hover:text-green-600">
-            <a href="/">Home</a>
+            <Link to="/">Home</Link>
           </li>
           <li className=" hover:text-green-600">
-            <a href="/portfolio">Portfolio</a>
+            <Link to="/portfolio">Portfolio</Link>
           </li>
           <li className=" hover:text-green-600">
-            <a href="/experience">Experience</a>
+            <Link to="/experience">Experience</Link>
           </li>
           <li className=" hover:text-green-600">
             <a href="#contact-me-id">Contact Me</a>
